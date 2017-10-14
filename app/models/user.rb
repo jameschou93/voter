ab56aef4279d4da0e70cabea6ecdffde9a9e0268
James Chou <jameschou1993@gmail.com>
2017-10-14 15:48:25 -0400

    added accessor to all avatar attributes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
  acts_as_voter
  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
- attr_accessor(:avatar_file_name)
+ attr_accessor :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at
+
 end
