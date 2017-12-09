d27466a838d66e746c8a311b2bbce6a6064e7783
James Chou <james.chou.career@gmail.com>
2017-12-09 13:37:49 -0500

    removed erroneous migration files

--- db/migrate/20170907211156_remove_votes_from_ideas.rb
+++
@@ -1,6 +0,0 @@
-class RemoveVotesFromIdeas < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :ideas, :up_vote, :integer, :default => 0
-    remove_column :ideas, :down_vote, :integer, :default => 0
-  end
-end
--- db/migrate/20170910195546_drop_votes.rb
+++
@@ -1,5 +0,0 @@
-class DropVotes < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :votes
-  end
-end
